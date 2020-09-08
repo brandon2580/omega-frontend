09b2a5dd445a364e4a5ecc8441e4f4336c860a32
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2020-09-07 23:21:03 -0700

    Moved tests to __tests__

--- src/__tests__/sum.js
+++ src/__tests__/sum.js
@@ -1,7 +1,8 @@
+import App from "../App"
 function sum(a, b) {
     return a + b;
   }
 
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3)
   });
