2de0d34092cd69e1fceaceb59b54837568a2a730
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2020-09-11 00:36:30 -0700

    Removed Toggle, wasn't used

--- src/Components/DarkModeToggle.js
+++ src/Components/DarkModeToggle.js
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
-import Toggle from '../Toggle';
-import useDarkMode from 'use-dark-mode';
+import React from "react";
+import useDarkMode from "use-dark-mode";
 import DarkModeToggle from "react-dark-mode-toggle";
 
 const DarkModeToggler = () => {
-    const darkMode = useDarkMode(true);
+  const darkMode = useDarkMode(true);
 
-    return (
-        <div className="dark-mode-toggle">
-            <DarkModeToggle
-                onChange={darkMode.toggle}
-                speed={2}
-                checked={darkMode.value}
-                size={80}
-            />
-
-        </div>
-    );
+  return (
+    <div className="dark-mode-toggle">
+      <DarkModeToggle
+        onChange={darkMode.toggle}
+        speed={2}
+        checked={darkMode.value}
+        size={80}
+      />
+    </div>
+  );
 };
 
 export default DarkModeToggler;
