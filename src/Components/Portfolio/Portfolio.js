c63452d56810128692aacb2b015f7d66665b4380
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2020-09-20 00:06:31 -0700

    Sidenavbar visible on portfolio page

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../../App.scss";
+import Sidenavbar from "../Navbars/Sidenavbar";
 
 const Portfolio = () => {
   return (
     <div>
       <h1 className="center header">Portfolio Page</h1>
+      <Sidenavbar />
     </div>
   );
 };
