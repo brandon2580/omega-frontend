d111cdb9f9db13e7047f92fd3d71fe91f21fc5bf
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2020-12-01 21:50:43 -0800

    modal now appears when the user wants to save a layout (instead of an ugly popover like before)

--- src/Components/EquityDashboard/SaveLayoutButton.js
+++ src/Components/EquityDashboard/SaveLayoutButton.js
@@ -1,22 +1,46 @@
-import React from "react";
-import { Popover } from "antd";
+import React, { useState } from "react";
+import { Modal } from "antd";
 
 const SaveLayoutButton = (props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // Shows modal
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  // Handles exit of modal
+  const handleExit = (e) => {
+    setModalVisible(false);
+  };
+
   return (
     <div>
-      <Popover
-        content={
-          <form onSubmit={props.saveLayout}>
-            <input type="text" placeholder="Layout Name" onChange={props.handleChange} />
-          </form>
-        }
-        title="Layout Name"
-        trigger="click"
+      <button className="btn btn-primary" onClick={showModal}>
+        Save Layout
+      </button>
+      <Modal
+        title="Save Layout"
+        className="save-layout-modal"
+        footer={null}
+        visible={modalVisible}
+        onCancel={handleExit}
       >
-        <button className="btn btn-primary">Save Layout</button>
-      </Popover>
+        <form
+          className="form-inline ml-auto col-lg-9"
+          onSubmit={props.saveLayout}
+        >
+          <input
+            type="text"
+            className="react-autosuggest__input"
+            placeholder="Layout Name"
+            onChange={props.handleChange}
+          />
+        </form>
+        {props.wasTaken && <h5 className='error-message'>Name already in use, please try another</h5>}
+      </Modal>
 
-      {props.wasTaken && <h1>Name already in use, please try another</h1>}
+      
     </div>
   );
 };
