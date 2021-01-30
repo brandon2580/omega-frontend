7bf55b64dcf5dea0b35b035570ea54ceac63d2cf
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2021-01-29 20:54:17 -0800

    tried binning

--- src/Components/Cards/PriceCalendar.js
+++ src/Components/Cards/PriceCalendar.js
@@ -2,92 +2,101 @@ import React, { useEffect, useState } from "react";
 import "../../App.scss";
 import { Card } from "antd";
 import _ from "lodash";
-import ReactApexChart from "react-apexcharts";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts/core";
+import Radar from "fusioncharts/viz/radar";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, Radar, FusionTheme);
 
 const PriceCalendar = (props) => {
-  const [series, setSeries] = useState([{}]);
+  const [series, setSeries] = useState([]);
+
+  function percentChange(n1, n2) {
+    return (((n2 - n1) / n1) * 100).toFixed(2);
+  }
 
   useEffect(() => {
-    let lastFourWeeks = props.data.slice(Math.max(props.data.length - 20, 1));
-
-    function groupArr(data, n) {
-      var group = [];
-      for (var i = 0, j = 0; i < data.length; i++) {
-        if (i >= n && i % n === 0) j++;
-        group[j] = group[j] || [];
-        group[j].push(data[i]);
-      }
-      return group;
-    }
-
-    let grouped = groupArr(lastFourWeeks, 5);
-    let mapped = grouped.map((el) => {
-      let percentChange = el.map((price) => {
-        return {
-          x: price.x,
-          y: price.change,
-        };
-      });
-
-      return {
-        data: percentChange,
-      };
+    let closes = props.data.slice(2).map((el) => {
+      return el.close;
     });
+
+    let months = props.data.slice(2).map((el) => {
+      let month = el.x.substring(5, 7)
+      return month
+    })
+
+    let grouped = _.zip(closes, _.tail(closes))
+
+    let mapped = grouped.map((price) => {
+      return percentChange(price[0], price[1])
+    });
+    mapped.pop();
+
+    console.log(grouped)
+    console.log(mapped)
+    console.log(props.data.slice(2))
 
     setSeries(mapped);
   }, [props.data]);
 
-  let options = {
-    plotOptions: {
-      heatmap: {
-        colorScale: {
-          ranges: [
-            {
-              from: -100,
-              to: -3,
-              color: "#8B0000",
-              name: "lower",
-            },
-            {
-              from: -2.99,
-              to: -0.25,
-              color: "#FF0000",
-              name: "low",
-            },
-            {
-              from: -0.24,
-              to: 0.25,
-              color: "#C0C0C0",
-              name: "neutral",
-            },
-            {
-              from: 0.26,
-              to: 2.99,
-              color: "#00FF00",
-              name: "high",
-            },
-            {
-              from: 3,
-              to: 100,
-              color: "#009900",
-              name: "higher",
-            },
-          ],
-        },
-      },
-    },
+
+  const dataSource = {
     chart: {
-      height: 350,
-      type: "heatmap",
+      showlegend: "0",
+      showdivlinevalues: "0",
+      showlimits: "0",
+      showvalues: "1",
+      plotfillalpha: "40",
     },
-    xaxis: {
-      type: "category",
-      categories: ["Mon", "Tues", "Wed", "Thur", "Fri"],
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    colors: ["#008FFB"],
+    categories: [
+      {
+        category: [
+          {
+            label: "Jan",
+          },
+          {
+            label: "Feb",
+          },
+          {
+            label: "Mar",
+          },
+          {
+            label: "Apr",
+          },
+          {
+            label: "May",
+          },
+          {
+            label: "Jun",
+          },
+          {
+            label: "Jul",
+          },
+          {
+            label: "Aug",
+          },
+          {
+            label: "Sep",
+          },
+          {
+            label: "Oct",
+          },
+          {
+            label: "Nov",
+          },
+          {
+            label: "Dec",
+          },
+        ],
+      },
+    ],
+    dataset: [
+      {
+        seriesname: "% Change",
+        data: series,
+      },
+    ],
   };
 
   return (
@@ -103,11 +112,12 @@ const PriceCalendar = (props) => {
     >
       <hr className="card-hr" />
       <div style={{ height: "456px" }}>
-        <ReactApexChart
-          options={options}
-          series={series}
-          type="heatmap"
-          height="456"
+        <ReactFC
+          type="radar"
+          width="100%"
+          height="80%"
+          dataFormat="JSON"
+          dataSource={dataSource}
         />
       </div>
     </Card>
