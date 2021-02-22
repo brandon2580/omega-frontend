e690d467db7a42b9c4554677319b05d75d3735b3
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2021-02-21 21:38:50 -0800

    Formatted values below logo

--- src/Components/EquityDashboard/TickerHeader.js
+++ src/Components/EquityDashboard/TickerHeader.js
@@ -3,11 +3,12 @@ import { Card, Popover } from "antd";
 import "../../App.scss";
 
 const TickerHeader = (props) => {
-  const [ticker, setTicker] = useState('')
+  const [ticker, setTicker] = useState("");
 
   useEffect(() => {
-    setTicker(props.tickerCard.ticker)
-  }, [props.tickerCard.ticker])
+    setTicker(props.tickerCard.ticker);
+  }, [props.tickerCard.ticker]);
+
 
   return (
     <div className="row">
@@ -18,13 +19,18 @@ const TickerHeader = (props) => {
           <div className="ticker-information">
             <div className="row">
               <div className="col-lg-3 justify-content">
-                <h1 className="ticker-title">{props.tickerCard.company_name}</h1>
+                <h1 className="ticker-title">
+                  {props.tickerCard.company_name}
+                </h1>
               </div>
             </div>
 
             <div className="row">
               <div className="col-lg-3 justify-content">
-                <img style={{ borderRadius: "1000px" }} src={`https://storage.googleapis.com/iex/api/logos/${ticker}.png`} />
+                <img
+                  style={{ borderRadius: "1000px" }}
+                  src={`https://storage.googleapis.com/iex/api/logos/${ticker}.png`}
+                />
               </div>
               <div className="col-lg-3 ">
                 <p>{props.tickerCard.industry}</p>
@@ -32,7 +38,9 @@ const TickerHeader = (props) => {
                 <p>
                   CEO: {props.tickerCard.ceo}
                   <br />
-                  <a href={props.tickerCard.website}>{props.tickerCard.website}</a>
+                  <a href={props.tickerCard.website}>
+                    {props.tickerCard.website}
+                  </a>
                   <br />
                   {props.tickerCard.country}
                 </p>
@@ -42,17 +50,17 @@ const TickerHeader = (props) => {
             <div className="row">
               <div className="col-lg-1 justify-content">
                 <p className="center">
-                  Market Cap: <br /> {props.tickerCard.market_cap}
+                  Market Cap: <br /> ${props.tickerCard.market_cap}
                 </p>
               </div>
               <div className="col-lg-1 justify-content">
                 <p className="center">
-                  Total Return: <br /> {props.tickerCard.total_return}
+                  Total Return: <br /> {props.tickerCard.total_return}x
                 </p>
               </div>
               <div className="col-lg-1 justify-content">
                 <p className="center">
-                  Price: <br /> {props.tickerCard.price}
+                  Price: <br /> ${props.tickerCard.price}
                 </p>
               </div>
               <div className="col-lg-9 justify-content">
